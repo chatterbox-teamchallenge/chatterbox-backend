8e1583c7e84875220d7b2beb25f1af5f04f9c58a
Arthur Kozub <kozub0246@gmail.com>
2024-05-11 11:16:02 +0300

    feat: login/signup api implementation - basic

---
+++ dist/app.js
@@ -0,0 +1,25 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const userRoutes_1 = __importDefault(require("./routes/userRoutes"));
+const database_1 = __importDefault(require("./database"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+const PORT = process.env.PORT;
+app.use(express_1.default.json());
+app.use('/users', userRoutes_1.default);
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+});
+app.get('/', (req, res) => {
+    res.send('The app is working, keep grinding!');
+});
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
+(0, database_1.default)();
---
+++ src/app.ts
@@ -0,0 +1,27 @@
+import express, { Application, Request, Response, NextFunction } from 'express';
+import userRoutes from './routes/userRoutes';
+import connectDB from './database';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app: Application = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+
+app.use('/users', userRoutes);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+});
+
+app.get('/', (req, res) => {
+    res.send('The app is working, keep grinding!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
+
+connectDB();
