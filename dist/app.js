ca9be06bb623c72150b3e8e79be381cb7bdaaed6
Arthur Kozub <kozub0246@gmail.com>
2024-05-27 10:49:53 +0300

    forget to compile

--- dist/app.js
+++ dist/app.js
@@ -12,11 +12,7 @@ dotenv_1.default.config();
 const app = (0, express_1.default)();
 const PORT = process.env.PORT;
 app.use(express_1.default.json());
-app.use((0, cors_1.default)({
-    origin: 'http://localhost:' + PORT,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true
-}));
+app.use((0, cors_1.default)());
 app.use('/users', userRoutes_1.default);
 app.use((err, req, res, next) => {
     console.error(err.stack);
