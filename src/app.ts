11e8769512a865d636df2347f947a7bfb0393787
Arthur Kozub <kozub0246@gmail.com>
2024-05-27 10:29:58 +0300

    fix: cors allow

--- src/app.ts
+++ src/app.ts
@@ -10,11 +10,7 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 
-app.use(cors({
-    origin: "http://localhost:3000"
-}));
-
-app.options('*', cors())
+app.use(cors());
 
 app.use('/users', userRoutes);
 
